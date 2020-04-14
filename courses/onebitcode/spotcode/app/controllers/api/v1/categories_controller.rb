7a096256ee202e1a8c0c9376e50606ec1774786a
eopit <pedro.barricelli@gmail.com>
2020-04-14 16:01:45 -0300

    added 80% backend spotify

---
+++ courses/onebitcode/spotcode/app/controllers/api/v1/categories_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class Api::V1::CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+end
